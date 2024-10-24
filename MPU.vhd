95ebbe7c835cd00245fdb8e613081e26b78772fd
Sam0929 <samuel.v@puccampinas.edu.br>
2024-10-24 18:42:38 -0300

    Memória modificada

--- MPU.vhd
+++ MPU.vhd
@@ -4,38 +4,37 @@ use IEEE.STD_LOGIC_ARITH.ALL;
 use IEEE.STD_LOGIC_UNSIGNED.ALL;
 
 entity MPU is
+
     Port (
         ce_n, we_n, oe_n: in std_logic;
         intr: out std_logic;
         address: in std_logic_vector(15 downto 0);
         data: inout std_logic_vector(15 downto 0);
-		  clk: in std_logic;
-		  reset: in std_logic
+		clk: in std_logic;
+		reset: in std_logic
 		  
     );
+	 
 end MPU;
 
 architecture Behavioral of MPU is
     
-    type matrix is array (3 downto 0, 3 downto 0) of std_logic_vector(15 downto 0); -- Matriz 4x4 de 16 bits
-    signal A, B, C: matrix := (others => (others => (others => '0')));  -- Inicializa as matrizes com zero
-    signal command: std_logic_vector(15 downto 0) := (others => '0');  -- Inicializa o comando
-    signal temp_result: std_logic_vector(31 downto 0);  -- Resultado temporário
-    signal row, col: integer range 0 to 3;  -- Índices para percorrer as linhas e colunas
+	type Matrix is array (63 downto 0) of std_logic_vector(15 downto 0);
+    signal memory: Matrix;  
+   
+   --signal temp_result: std_logic_vector(31 downto 0);  -- Resultado temporário
+	
+    signal i: integer range 0 to 15;  														-- Índices para percorrer as matrizes
+    signal iA, iB, iC, memory_aux: integer range 0 to 63;
 
     -- Definindo os estados da FSM
-    type state_type is (IDLE, READ_CMD, EXECUTE, COMPLETE);
-    signal CURRENT_STATE, next_state: state_type;
+
+    type state_type is (IDLE, READ_WR, O_MATRIX, W_MATRIX, WRITE_DATA, DECODE, EXECUTE, COMPLETE);
+    signal CURRENT_STATE: state_type;
+	
     signal operation: std_logic_vector(2 downto 0) := (others => '0');  -- Tipo de operação (add, sub, etc.)
 
 begin
-
-    -- Atualiza o estado atual na borda do clock
-
-	
-
-		
-    -- FSM para controlar o fluxo de execução
 	 
     process(clk, reset)
 
@@ -56,45 +55,67 @@ begin
 						 
 						 if ce_n = '1' then
 						 
-							CURRENT_STATE <= CURRENT_STATE
+							CURRENT_STATE <= CURRENT_STATE;
 							
 						elsif ce_n = '0' then
 						
-							CURRENT_STATE <= DECODE
+							CURRENT_STATE <= READ_WR;
 							
 						end if;
 
-					when DECODE =>
+					when READ_WR =>
 					
 						if ce_n = '0' and we_n = '0' then
 						
-							CURRENT_STATE <= W_MATRIX
+							CURRENT_STATE <= W_MATRIX;
 							
 						elsif ce_n ='0' and oe_n = '0' then
 						
-							CURRENT_STATE <= O_MATRIX
+							CURRENT_STATE <= O_MATRIX;
 						
 						end if;
 					
 					when O_MATRIX =>
 						
+						memory_aux <= conv_integer(address);
+						
+						data <= memory(memory_aux);
+						
 					when W_MATRIX =>
 						
 						if address > "0000000000001111" then
 							
-							CURRENT_STATE <= WRITE_DATA
+							CURRENT_STATE <= WRITE_DATA;
 						
 						elsif address = "0000000000000000" then
 						
-							CURRENT_STATE <= DECODE
+							CURRENT_STATE <= DECODE;
 						
-						end if;						
+						end if;
+
+					when WRITE_DATA =>
+					
+						memory_aux <= conv_integer(address);        --verificar problema, quando chega nesse estado, a memoria escreve antes do memory_aux receber o indice
+						
+						memory(memory_aux) <= data;              
+									
 							
 					when DECODE =>
-						
-						case data is
-							
-							when data(15 downto 12) 
+            
+
+						case data (15 downto 12) is
+                
+                    when "0000" => operation <="000"; -- add
+                    when "0001" => operation <= "001"; -- sub
+                    when "0010" => operation <= "010"; -- mul
+                    when "0011" => operation <= "011"; -- mac
+                    when "0100" => operation <= "100"; -- fill
+                    when "0101" => operation <= "100"; -- identity
+                    when others => operation <= "111"; -- comando inválido
+
+						end case;
+
+               CURRENT_STATE <= EXECUTE;
 							
             when EXECUTE =>
 
@@ -102,127 +123,129 @@ begin
 					 
                     when "000" =>  -- ADD
 
-                        for row in 0 to 3 loop
+                        for i in 0 to 15 loop
 
-                            for col in 0 to 3 loop
+                            iA <= i + 32; iB <= i + 48 ; iC <= i + 16; 
 
-                                C(row, col) <= A(row, col) + B(row, col);  -- Soma matriz A e B
+                            memory(iC) <= memory(iA) + memory(iB);
 
-                            end loop;
-
+										
                         end loop;
 
                     when "001" =>  -- SUB
 
-                        for row in 0 to 3 loop
+                        for i in 0 to 15 loop
 
-                            for col in 0 to 3 loop
+                            iA <= i + 32; iB <= i + 48 ; iC <= i + 16; 
 
-                                C(row, col) <= A(row, col) - B(row, col);  -- Subtrai matriz B de A
+                            memory(iC) <= memory(iA) - memory(iB);
 
-                            end loop;
 
                         end loop;
 
-                    when "010" =>  -- MUL
+                -- NECESSARIO IMPLEMENTAR        
 
-                        for row in 0 to 3 loop
+                --     when "010" =>  -- MUL                      
 
-                            for col in 0 to 3 loop
+                --     for row in 0 to 3 loop
 
-                                temp_result <= (others => '0');
+                --         for col in 0 to 3 loop
 
-                                for k in 0 to 3 loop
+                --             temp_result <= (others => '0');
 
-                                    temp_result <= temp_result + A(row, k) * B(k, col);
+                --             for k in 0 to 3 loop
 
-                                end loop;
+                --                 temp_result <= temp_result + A(row, k) * B(k, col);
 
-                                C(row, col) <= temp_result(15 downto 0);
+                --             end loop;
 
-                            end loop;
+                --             C(row, col) <= temp_result(15 downto 0);
+
+                --         end loop;
 
 
-                           
-                        end loop;
+                        
+                --     end loop;
 
-                    when "011" =>  -- MAC (C = C + A * B)
+                -- when "011" =>  -- MAC (C = C + A * B)
 
-                        for row in 0 to 3 loop
+                --     for row in 0 to 3 loop
 
-                            for col in 0 to 3 loop
+                --         for col in 0 to 3 loop
 
-                                temp_result <= (others => '0');
+                --             temp_result <= (others => '0');
 
-                                for k in 0 to 3 loop
+                --             for k in 0 to 3 loop
 
-                                    temp_result <= temp_result + A(row, k) * B(k, col);
+                --                 temp_result <= temp_result + A(row, k) * B(k, col);
 
-                                end loop;
+                --             end loop;
 
-                                C(row, col) <= C(row, col) + temp_result(15 downto 0);
+                --             C(row, col) <= C(row, col) + temp_result(15 downto 0);
 
-                            end loop;
+                --         end loop;
 
-                        end loop;
+                --     end loop;
 
-                    when "100" =>  -- FILL (preencher matriz com valor)
+                -- when "100" =>  -- FILL (preencher matriz com valor)
 
-                        for row in 0 to 3 loop
+                --     for row in 0 to 3 loop
 
-                            for col in 0 to 3 loop
+                --         for col in 0 to 3 loop
 
-                                C(row, col) <= (others => '1');  -- Exemplo: preenche com 1s
+                --             C(row, col) <= (others => '1');  -- Exemplo: preenche com 1s
 
-                            end loop;
+                --         end loop;
 
-                        end loop;
+                --     end loop;
 
-                    when "101" =>  -- IDENTITY (matriz identidade)
+                -- when "101" =>  -- IDENTITY (matriz identidade)
 
-                        for row in 0 to 3 loop
+                --     for row in 0 to 3 loop
 
-                            for col in 0 to 3 loop
+                --         for col in 0 to 3 loop
 
-                                if row = col then
+                --             if row = col then
 
-                                    C(row, col) <= "0000000000000001"; -- 1s na diagonal
+                --                 C(row, col) <= "0000000000000001"; -- 1s na diagonal
 
-                                else
+                --             else
 
-                                    C(row, col) <= (others => '0'); -- 0s nas outras posições
+                --                 C(row, col) <= (others => '0'); -- 0s nas outras posições
 
-                                end if;
+                --             end if;
 
-                            end loop;
+                --         end loop;
 
-                        end loop;
+                --     end loop;
+
+                
 
                     when others =>  -- Comando inválido
 
-                        for row in 0 to 3 loop
+                        for i in 0 to 15 loop
 
-                            for col in 0 to 3 loop
+                                iC <= i + 16; 
 
-                                C(row, col) <= (others => '0');  -- ZERA A MATRIZ
+                                memory(iC) <= (others =>'0');
 
-                            end loop;
 
                         end loop;
 
+
                 end case;
 
-                next_state <= COMPLETE;
+                CURRENT_STATE <= COMPLETE;
 
             when COMPLETE =>
 
                 intr <= '1';  -- Levanta o sinal de interrupção
 
-                next_state <= IDLE;  -- Volta para o estado de espera (IDLE)
+                CURRENT_STATE <= IDLE;  -- Volta para o estado de espera (IDLE)
 
             when others =>
 
-                next_state <= IDLE;
+                CURRENT_STATE <= IDLE;
 
         end case;
 		  
