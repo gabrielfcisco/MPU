7dbb61706a4e18c14371843ba47e6cb481de9814
Sam0929 <samuel.v@puccampinas.edu.br>
2024-10-27 14:03:13 -0300

    Correções na indentação

--- MPU.vhd
+++ MPU.vhd
@@ -3,6 +3,7 @@ use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.NUMERIC_STD.ALL;
 
 entity MPU is
+
     Port (
         ce_n, we_n, oe_n: in std_logic;
         intr: out std_logic;
@@ -11,14 +12,16 @@ entity MPU is
         clk: in std_logic;
         reset: in std_logic
     );
+
 end MPU;
 
 architecture Behavioral of MPU is
 
     type Matrix is array (63 downto 0) of std_logic_vector(15 downto 0);
-
     signal memory: Matrix;  
+
     signal temp_result: std_logic_vector(31 downto 0);  -- Resultado temporário
+
     signal i: integer range 0 to 15;                   -- Índices para percorrer as matrizes
     signal j, k: integer range 0 to 3;
 	signal memory_aux: integer range 0 to 63;
@@ -32,52 +35,80 @@ architecture Behavioral of MPU is
     signal inteiro: std_logic_vector(15 downto 0) := (others => '0');  -- Sinal do inteiro para o FILL
 
 begin
+
     process(clk, reset)
+
     begin
+
         if reset = '1' then
+
             for iA in 0 to 63 loop
+
                 memory(iA) <= (others => '0');
+
             end loop;
 
             CURRENT_STATE <= IDLE;
+
             intr <= '0';
 
         elsif rising_edge(clk) then
+
             case CURRENT_STATE is
 
                 when IDLE =>
+
                     intr <= '0';
 
                     if ce_n = '1' then
+
                         CURRENT_STATE <= CURRENT_STATE;
+
                     elsif ce_n = '0' then
+
                         CURRENT_STATE <= READ_WR;
+
                     end if;
 
                 when READ_WR =>
+
                     if ce_n = '0' and we_n = '0' then
+
                         CURRENT_STATE <= W_MATRIX;
+
                     elsif ce_n ='0' and oe_n = '0' then
+
                         CURRENT_STATE <= O_MATRIX;
+
                     end if;
 
                 when O_MATRIX =>
+
                     memory_aux <= to_integer(unsigned(address));
+                    
                     data <= memory(memory_aux);
 
                 when W_MATRIX =>
+
                     if address > "0000000000001111" then
+
                         CURRENT_STATE <= WRITE_DATA;
+
                     elsif address = "0000000000000000" then
+
                         CURRENT_STATE <= DECODE;
+
                     end if;
 
                 when WRITE_DATA =>
+
                     memory_aux <= to_integer(unsigned(address));  -- verificar problema
                     memory(memory_aux) <= data;
 
                 when DECODE =>
+
                     case data(15 downto 13) is
+
                         when "000" => operation <= "000"; -- add
                         when "001" => operation <= "001"; -- sub
                         when "010" => operation <= "010"; -- mul
@@ -85,6 +116,7 @@ begin
                         when "100" => operation <= "100"; -- fill
                         when "101" => operation <= "101"; -- identity
                         when others => operation <= "111"; -- comando inválido
+
                     end case;
 
                     CURRENT_STATE <= EXECUTE;
@@ -116,17 +148,17 @@ begin
 
                             for i in 0 to 3 loop
 									 
-                                temp_result <= (others => '0');
+                                    temp_result <= (others => '0');
 
                                 for j in 0 to 3 loop
 										  
-												for k in 0 to 3 loop
+                                    for k in 0 to 3 loop
+                                    
+                                        temp_result <= std_logic_vector(unsigned(temp_result) + unsigned(memory(i*4 + j + 32)) * unsigned(memory(48 + j + k*4)));
+                                    
+                                    end loop;
 												
-													temp_result <= std_logic_vector(unsigned(temp_result) + unsigned(memory(i*4 + j + 32)) * unsigned(memory(48 + j + k*4)));
-												
-												end loop;
-												
-											memory(i*4 + j + 16) <= temp_result(15 downto 0);
+								    memory(i*4 + j + 16) <= temp_result(15 downto 0);
 												
                                 end loop;
 										  
@@ -135,64 +167,64 @@ begin
 
                         when "011" =>  -- MAC (MUL + C)
 								
-									for i in 0 to 3 loop
+							for i in 0 to 3 loop
 									 
                                 temp_result <= (others => '0');
 
                                 for j in 0 to 3 loop
 										  
-												for k in 0 to 3 loop
-												
-													temp_result <= std_logic_vector(unsigned(temp_result) + unsigned(memory(i*4 + j + 32)) * unsigned(memory(48 + j + k*4)));
-												
-												end loop;
-												
-											memory(i*4 + j + 16) <= std_logic_vector(unsigned(memory(i*4 + j + 16)) + unsigned(temp_result(15 downto 0)));
+                                    for k in 0 to 3 loop
+                                    
+                                        temp_result <= std_logic_vector(unsigned(temp_result) + unsigned(memory(i*4 + j + 32)) * unsigned(memory(48 + j + k*4)));
+                                    
+                                    end loop;
+                                    
+                                    memory(i*4 + j + 16) <= std_logic_vector(unsigned(memory(i*4 + j + 16)) + unsigned(temp_result(15 downto 0)));
 												
                                 end loop;
 										  
                             end loop;
 				 
 									 
-							   when "100" =>  -- FILL (preencher matriz com valor)
-                        
-									case data (12 downto 11) is
-            
-										when "00" => 
+                        when "100" =>  -- FILL (preencher matriz com valor)
+                
+                            case data (12 downto 11) is
+    
+                                when "00" => 
 
-											inteiro <= "00000"&data(10 downto 0); 
+                                    inteiro <= "00000"&data(10 downto 0); 
 
-											for i in 0 to 15 loop
-                        
-												memory(i + 32) <= inteiro;  --iA
-                            
-											end loop;
+                                    for i in 0 to 15 loop
+                
+                                        memory(i + 32) <= inteiro;  --iA
+                    
+                                    end loop;
 
-										when "01" =>
+                                when "01" =>
 
-											inteiro <= "00000"&data(10 downto 0);
+                                    inteiro <= "00000"&data(10 downto 0);
 
-											for i in 0 to 15 loop
-                        
-												memory(i + 48) <= inteiro;   --iB
-                            
-											end loop;
+                                    for i in 0 to 15 loop
+                
+                                        memory(i + 48) <= inteiro;   --iB
+                    
+                                    end loop;
 
-										when "10" =>
+                                when "10" =>
 
-											inteiro <= "00000"&data(10 downto 0); 
+                                    inteiro <= "00000"&data(10 downto 0); 
 
-											for i in 0 to 15 loop
-                        
-												memory(i + 16) <= inteiro;   --iC
-                            
-											end loop;
-											
-										when others =>
-										
-											CURRENT_STATE <= COMPLETE; -- NÃO FAZ NADA
+                                    for i in 0 to 15 loop
+                
+                                        memory(i + 16) <= inteiro;   --iC
+                    
+                                    end loop;
+                                    
+                                when others =>
+                                
+                                    CURRENT_STATE <= IDLE; -- NÃO FAZ NADA
 
-									end case;
+                            end case;
 									
 
                         when "101" =>  -- IDENTITY (matriz identidade)
@@ -234,7 +266,9 @@ begin
                     CURRENT_STATE <= IDLE;
 
             end case;
+
         end if;
+
     end process;
 
 end Behavioral;
